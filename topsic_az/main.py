0e3b428f3138870aa451cbc9086cd091bb193ae5
kotabando <kota_bando@pj100.biz>
2022-04-26 18:12:33 +0900

    マス目の中に文字を追加

---
+++ topsic_az/main.py
@@ -0,0 +1,11 @@
+H, W, Q = map(int,input().split())
+rcs = [input().split() for _ in range(Q)]
+
+x = [["."] * W for _ in range(H)]
+print(x)
+print(rcs)
+for i in range(Q):
+    x[int(rcs[i][0]) - 1][int(rcs[i][1]) - 1] = rcs[i][2]
+
+for j in x:
+    print(*j)
