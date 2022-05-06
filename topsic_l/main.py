fd3be1943bf4741a82f33d75616a33a632740c80
kotabando <kota_bando@pj100.biz>
2022-05-06 09:12:12 +0900

    三角形の相似

--- topsic_l/main.py
+++ topsic_l/main.py
@@ -1,6 +1,7 @@
 xy = [list(map(int,input().split())) for _ in range(6)]
 
 from math import dist
+import math
 
 a = dist(xy[0], xy[1]), dist(xy[1], xy[2]), dist(xy[2], xy[0])
 b = dist(xy[3], xy[4]), dist(xy[4], xy[5]), dist(xy[5], xy[3])
@@ -8,7 +9,7 @@ b = dist(xy[3], xy[4]), dist(xy[4], xy[5]), dist(xy[5], xy[3])
 list_a = sorted(list(a))
 list_b = sorted(list(b))
 
-if list_a[1] / list_a[0] == list_b[1] / list_b[0] and list_a[2] / list_a[1] == list_b[2] / list_b[1] and list_a[0] / list_a[2] == list_b[0] / list_b[2]:
+if math.isclose(list_a[1] / list_a[0], list_b[1] / list_b[0]) and math.isclose(list_a[2] / list_a[1], list_b[2] / list_b[1]) and math.isclose(list_a[0] / list_a[2], list_b[0] / list_b[2]):
     print("YES")
 else:
     print("NO")
