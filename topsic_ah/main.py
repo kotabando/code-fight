ca7c60351a7e21575d3ff494f4b95bc47070f01e
kotabando <kota_bando@pj100.biz>
2022-05-10 10:50:12 +0900

    アスタリスクな道路

--- topsic_ah/main.py
+++ topsic_ah/main.py
@@ -1,16 +1,17 @@
 import math
+
 N, M = map(int,input().split())
 ab = [list(map(int,input().split())) for _ in range(M)]
 
 count = 0
 
 for i in range(1, N+1):
-    list_ab = []
+    ab_list = []
     for j in range(M):
         if i in ab[j]:
-            list_ab.append(ab[j])
+            ab_list.append(ab[j])
 
-    if len(list_ab) >= 6:
-        count += math.factorial(len(list_ab)) // math.factorial(6) * math.factorial(len(list_ab) - 6)
+    if len(ab_list) >= 6:
+        count += math.factorial(len(ab_list)) // (math.factorial(len(ab_list) - 6) * math.factorial(6))
 
-print(math.factorial(6) * count)
+print(count * math.factorial(6))
